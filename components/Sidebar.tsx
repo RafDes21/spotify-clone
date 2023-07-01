e9dacc71eb2176b80adbc50769da39ed9c1d3ae4
RafDes21 <israeltineo84@gmail.com>
2023-07-01 18:34:59 -0300

    add Box component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,30 +1,40 @@
-"use client"
-import {usePathname} from "next/navigation"
+"use client";
+import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import Box from "./Box";
 
 interface SidebarProps {
   children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
 
-   const pathname=  usePathname()
+  const routes = useMemo(
+    () => [
+      {
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    [pathname]
+  );
 
-const routes = useMemo(()=>[
-{
-    label: 'Home',
-    active: pathname !=='/search',
-    href: '/'
-},
-{
-    label: 'Search',
-    active: pathname ==='/search',
-    href: '/search'
-}
-
-], [pathname])
-
-  return <div>{children}</div>;
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          siber navigation
+        </Box>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
